4b10c217c27be5a99ba2d1fa7c1cc54ef4aa9e10
Kirui-brian <brinkiru@gmail.com>
2024-10-22 08:42:22 +0300

    add pagination of 10

--- laravel-blog/app/Http/Controllers/PostController.php
+++ laravel-blog/app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     // Get all posts
      public function index()
     {
-        $posts = Post::with(['author', 'tags'])->latest()->get();
+        $posts = Post::with(['author', 'tags'])->latest()->get()->paginate(10);
         return response()->json($posts);
         // return Post::all();
     }
